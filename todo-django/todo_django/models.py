43283c781f7df2cb5ff76b82d6f429837f747c0d
waffledood <itshaikalhere@hotmail.sg>
2024-02-06 22:20:20 +0800

    Define TodoItem model

--- todo-django/todo_django/models.py
+++ todo-django/todo_django/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class TodoItem(models.Model):
+    detail = models.CharField(max_length=128)
+    completed = models.BooleanField(default=False)
