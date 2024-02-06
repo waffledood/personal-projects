7ac7def0a3b1d1de4a796e5c6e8ea1c7f25fb848
waffledood <itshaikalhere@hotmail.sg>
2024-02-06 22:39:12 +0800

    Define str for TodoItem model

--- todo-django/todo_django/models.py
+++ todo-django/todo_django/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class TodoItem(models.Model):
     detail = models.CharField(max_length=128)
     completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"[{'X' if self.completed else ' '}] Task ({self.id}): {self.detail}"
