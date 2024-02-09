c5dc4c568c799427d2060bd1e07ec1841b0607d4
waffledood <itshaikalhere@hotmail.sg>
2024-02-09 10:06:09 +0800

    Create /addTodoItem endpoint

--- todo-django/todo_django/views.py
+++ todo-django/todo_django/views.py
@@ -1,6 +1,10 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+import json
 
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
+from .forms import TodoItemForm
 from .models import TodoItem
 
 
@@ -13,3 +17,33 @@ def index(request):
         template_name="todo_django/index.html",
         context={"allTodoItemObjects": allTodoItemObjects},
     )
+
+
+@csrf_exempt
+def addTodoItem(request):
+    if request.method != "POST":
+        return JsonResponse(
+            data={"message": "Only POST requests are allowed."}, status=400
+        )
+
+    try:
+        jsonPostData = json.loads(request.body)
+
+        form = TodoItemForm(data=jsonPostData)
+        new_TodoItem = form.save()
+
+        print("new_TodoItem saved:", new_TodoItem)
+
+        return JsonResponse(
+            data={
+                "message": "To-Do Item successfully added.",
+                "todoItem": new_TodoItem.serialize(),
+            },
+            status=200,
+        )
+    except ValueError:
+        print("Error during form validation")
+
+        return JsonResponse(
+            data={"message": "Error during form validation."}, status=400
+        )
