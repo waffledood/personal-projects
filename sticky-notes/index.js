f715a45be0c4376d83a1c3027d3c33d840ab26ce
waffledood <itshaikalhere@hotmail.sg>
2024-03-29 19:03:46 +0800

    Refactor dragElement function
    Remove unnecessary if-else control flow

--- sticky-notes/index.js
+++ sticky-notes/index.js
@@ -5,13 +5,8 @@ function dragElement(element) {
     pos2 = 0,
     pos3 = 0,
     pos4 = 0;
-  if (document.getElementById(element.id + "header")) {
-    /* if present, the header is where you move the DIV from:*/
-    document.getElementById(element.id + "header").onmousedown = dragMouseDown;
-  } else {
-    /* otherwise, move the DIV from anywhere inside the DIV:*/
-    element.onmousedown = dragMouseDown;
-  }
+
+  element.onmousedown = dragMouseDown;
 
   function dragMouseDown(e) {
     e = e || window.event;
