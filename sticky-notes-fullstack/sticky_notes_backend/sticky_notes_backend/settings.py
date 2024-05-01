21aa27f806bb365fdc520f539485886411ba7a72
Haikal Yusuf <itshaikalhere@hotmail.sg>
2024-05-01 16:37:24 +0800

    Configure sticky_notes_backend Django project for CORS & CSRF

--- sticky-notes-fullstack/sticky_notes_backend/sticky_notes_backend/settings.py
+++ sticky-notes-fullstack/sticky_notes_backend/sticky_notes_backend/settings.py
@@ -37,13 +37,17 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # 3rd party
     "rest_framework",
+    "corsheaders",
+    # local
     "stickynotes.apps.StickynotesConfig",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -123,3 +127,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "http://localhost:8000"]
+
+CSRF_TRUSTED_ORIGINS = ["localhost:3000"]
