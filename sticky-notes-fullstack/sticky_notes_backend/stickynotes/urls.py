6fb5780dfbdfb44cba2111be0329de9e3271bf92
Haikal Yusuf <itshaikalhere@hotmail.sg>
2024-05-01 16:09:22 +0800

    Refactor NoteAPIView class to ListNote

--- sticky-notes-fullstack/sticky_notes_backend/stickynotes/urls.py
+++ sticky-notes-fullstack/sticky_notes_backend/stickynotes/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import NoteAPIView, DetailNote
+from .views import ListNote, DetailNote
 
 urlpatterns = [
-    path(route="notes/", view=NoteAPIView.as_view(), name="note_list"),
+    path(route="notes/", view=ListNote.as_view(), name="note_list"),
     path(route="note/<int:pk>/", view=DetailNote.as_view(), name="note_detail"),
 ]
--- sticky-notes-fullstack/sticky_notes_backend/stickynotes/views.py
+++ sticky-notes-fullstack/sticky_notes_backend/stickynotes/views.py
@@ -7,7 +7,7 @@ from .models import Note
 from .serializers import NoteSerializer
 
 
-class NoteAPIView(generics.ListAPIView):
+class ListNote(generics.ListAPIView):
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
 
