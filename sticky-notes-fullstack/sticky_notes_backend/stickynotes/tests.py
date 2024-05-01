79792788e0842f1e13d33e627201c98d0dd26f2b
Haikal Yusuf <itshaikalhere@hotmail.sg>
2024-05-01 15:54:33 +0800

    Set up test for stickynotes app

--- sticky-notes-fullstack/sticky_notes_backend/stickynotes/tests.py
+++ sticky-notes-fullstack/sticky_notes_backend/stickynotes/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+from .models import Note
+
+
+class APITests(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.note = Note.objects.create(
+            text="This is my 1st sticky note", x_pixels_coord=120, y_pixels_coord=200
+        )
+
+    def test_api_listview(self):
+        response = self.client.get(reverse("note_list"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertContains(response, self.note.text)
