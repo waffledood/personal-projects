7dca8b040c239acaace67bc4843aa44108f6fefa
Haikal Yusuf <itshaikalhere@hotmail.sg>
2024-05-06 21:23:18 +0800

    Define UserSerializer to define the User model

--- sticky-notes-fullstack/sticky_notes_backend/stickynotes/serializers.py
+++ sticky-notes-fullstack/sticky_notes_backend/stickynotes/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 from .models import Note
@@ -7,3 +8,9 @@ class NoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Note
         fields = ("id", "text", "author", "x_pixels_coord", "y_pixels_coord")
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id", "username")
