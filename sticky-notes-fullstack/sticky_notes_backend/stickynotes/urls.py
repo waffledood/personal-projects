53c322215963fcb5333909b3301bffe494aa5988
Haikal Yusuf <itshaikalhere@hotmail.sg>
2024-05-06 21:59:55 +0800

    Add author/<int:pk>/notes API endpoint

--- sticky-notes-fullstack/sticky_notes_backend/stickynotes/urls.py
+++ sticky-notes-fullstack/sticky_notes_backend/stickynotes/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from .views import ListNote, DetailNote, DetailAuthor
+from .views import ListNote, DetailNote, listAuthorNotes, DetailAuthor
 
 urlpatterns = [
     path(route="notes/", view=ListNote.as_view(), name="note_list"),
     path(route="note/<int:pk>/", view=DetailNote.as_view(), name="note_detail"),
+    path(
+        route="author/<int:pk>/notes/", view=listAuthorNotes, name="author_notes_list"
+    ),
     path(route="author/<int:pk>/", view=DetailAuthor.as_view(), name="author_detail"),
 ]
--- sticky-notes-fullstack/sticky_notes_backend/stickynotes/views.py
+++ sticky-notes-fullstack/sticky_notes_backend/stickynotes/views.py
@@ -1,7 +1,11 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.decorators import api_view
+from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
 
 from django.contrib.auth.models import User
-from django.http import HttpResponse
+
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
 from .models import Note
@@ -16,6 +20,34 @@ class ListNote(generics.ListCreateAPIView):
 class DetailNote(generics.RetrieveUpdateDestroyAPIView):
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
+
+
+@api_view(["GET", "POST"])
+def listAuthorNotes(request, pk):
+    try:
+        author = User.objects.get(id=pk)
+    except User.DoesNotExist:
+        return Response(
+            {"detail": "No such User exists"}, status=status.HTTP_404_NOT_FOUND
+        )
+    except User.MultipleObjectsReturned:
+        return Response(
+            {"detail": "Multiple Users with the same id exists"},
+            status=status.HTTP_404_NOT_FOUND,
+        )
+
+    if request.method == "GET":
+        notes = Note.objects.filter(author=author)
+        serializer = NoteSerializer(notes, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    if request.method == "POST":
+        data = JSONParser().parse(request)
+        serializer = NoteSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
 
 
 class DetailAuthor(generics.RetrieveUpdateDestroyAPIView):
