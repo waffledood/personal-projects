517aa95e7343bbd33d3a7e3d6190c15a1368ed6c
Haikal Yusuf <itshaikalhere@hotmail.sg>
2024-05-06 18:50:53 +0800

    Add author field to Note model

--- sticky-notes-fullstack/sticky_notes_backend/stickynotes/models.py
+++ sticky-notes-fullstack/sticky_notes_backend/stickynotes/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Note(models.Model):
     text = models.CharField(max_length=128)
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE)
     x_pixels_coord = models.IntegerField()
     y_pixels_coord = models.IntegerField()
 
