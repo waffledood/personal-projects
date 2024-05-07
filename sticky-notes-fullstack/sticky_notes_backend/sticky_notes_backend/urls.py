95a7ae89399597015375d4e432704ee0f3555f3c
Haikal Yusuf <itshaikalhere@hotmail.sg>
2024-05-07 21:50:35 +0800

    Include dj_rest_auth API endpoints

--- sticky-notes-fullstack/sticky_notes_backend/sticky_notes_backend/urls.py
+++ sticky-notes-fullstack/sticky_notes_backend/sticky_notes_backend/urls.py
@@ -21,5 +21,6 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
+    path("api/dj-rest-auth/", include("dj_rest_auth.urls")),
     path("api/", include("stickynotes.urls")),
 ]
