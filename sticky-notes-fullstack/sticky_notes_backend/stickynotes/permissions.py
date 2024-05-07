2e4e30609dc0877360261850b8c769ce08ce35a5
Haikal Yusuf <itshaikalhere@hotmail.sg>
2024-05-07 22:25:47 +0800

    Define IsUserOrReadOnly permission

--- sticky-notes-fullstack/sticky_notes_backend/stickynotes/permissions.py
+++ sticky-notes-fullstack/sticky_notes_backend/stickynotes/permissions.py
@@ -12,3 +12,16 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
             return True
 
         return obj.author == request.user
+
+
+class IsUserOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            return True
+        return False
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj == request.user
