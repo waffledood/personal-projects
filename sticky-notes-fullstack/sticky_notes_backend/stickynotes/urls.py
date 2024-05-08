c98e699326003c7510fdc3ac6d9309de7094a434
Haikal Yusuf <itshaikalhere@hotmail.sg>
2024-05-08 22:46:20 +0800

    Remove author/<int:pk/notes/ API endpoint

--- sticky-notes-fullstack/sticky_notes_backend/stickynotes/urls.py
+++ sticky-notes-fullstack/sticky_notes_backend/stickynotes/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path
 
-from .views import ListNote, DetailNote, listAuthorNotes, DetailAuthor
+from .views import ListNote, DetailNote, DetailAuthor
 
 urlpatterns = [
     path(route="notes/", view=ListNote.as_view(), name="note_list"),
     path(route="notes/<int:pk>/", view=DetailNote.as_view(), name="note_detail"),
-    path(
-        route="author/<int:pk>/notes/", view=listAuthorNotes, name="author_notes_list"
-    ),
     path(route="author/<int:pk>/", view=DetailAuthor.as_view(), name="author_detail"),
 ]
--- sticky-notes-fullstack/sticky_notes_backend/stickynotes/views.py
+++ sticky-notes-fullstack/sticky_notes_backend/stickynotes/views.py
@@ -24,35 +24,6 @@ class DetailNote(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = NoteSerializer
 
 
-@api_view(["GET", "POST"])
-@permission_classes([IsAuthorOrReadOnly])
-def listAuthorNotes(request, pk):
-    try:
-        author = User.objects.get(id=pk)
-    except User.DoesNotExist:
-        return Response(
-            {"detail": "No such User exists"}, status=status.HTTP_404_NOT_FOUND
-        )
-    except User.MultipleObjectsReturned:
-        return Response(
-            {"detail": "Multiple Users with the same id exists"},
-            status=status.HTTP_404_NOT_FOUND,
-        )
-
-    if request.method == "GET":
-        notes = Note.objects.filter(author=author)
-        serializer = NoteSerializer(notes, many=True)
-        return JsonResponse(serializer.data, safe=False)
-
-    if request.method == "POST":
-        data = JSONParser().parse(request)
-        serializer = NoteSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
-
-
 class DetailAuthor(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsUserOrReadOnly,)
     queryset = User.objects.all()
