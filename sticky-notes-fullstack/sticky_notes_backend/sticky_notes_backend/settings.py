dc16762d24896467931b95694673f94dec9b84fb
Haikal Yusuf <itshaikalhere@hotmail.sg>
2024-05-09 21:07:37 +0800

    Remove SessionAuthentication

--- sticky-notes-fullstack/sticky_notes_backend/sticky_notes_backend/settings.py
+++ sticky-notes-fullstack/sticky_notes_backend/sticky_notes_backend/settings.py
@@ -149,7 +149,6 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticatedOrReadOnly"
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
     ],
 }
