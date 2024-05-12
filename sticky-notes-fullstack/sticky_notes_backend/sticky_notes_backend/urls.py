1a1158dab73efb249dff52720c81eabf27fe57a1
Haikal Yusuf <itshaikalhere@hotmail.sg>
2024-05-12 20:18:13 +0800

    Add comment indicating URL for authentication for Browsable APIs

--- sticky-notes-fullstack/sticky_notes_backend/sticky_notes_backend/urls.py
+++ sticky-notes-fullstack/sticky_notes_backend/sticky_notes_backend/urls.py
@@ -20,6 +20,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    # enables log in for the Browsable APIs
     path("api-auth/", include("rest_framework.urls")),
     path("api/dj-rest-auth/", include("dj_rest_auth.urls")),
     path("api/dj-rest-auth/registration/", include("dj_rest_auth.registration.urls")),
