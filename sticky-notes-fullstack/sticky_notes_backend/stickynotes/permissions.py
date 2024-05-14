feedfb6881f2efd259676b4e10d6f442ac1a0a6f
Haikal Yusuf <itshaikalhere@hotmail.sg>
2024-05-14 08:55:15 +0800

    Modify DetailNote view to only allow the author to view a Note

--- sticky-notes-fullstack/sticky_notes_backend/stickynotes/permissions.py
+++ sticky-notes-fullstack/sticky_notes_backend/stickynotes/permissions.py
@@ -8,9 +8,6 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
         return False
 
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
         return obj.author == request.user
 
 
