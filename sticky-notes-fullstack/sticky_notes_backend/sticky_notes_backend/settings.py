dbbadfe52b1e257f26a0f28f6e3b43414b5456e3
Haikal Yusuf <itshaikalhere@hotmail.sg>
2024-05-15 22:58:34 +0800

    Define schema for drf-spectacular

--- sticky-notes-fullstack/sticky_notes_backend/sticky_notes_backend/settings.py
+++ sticky-notes-fullstack/sticky_notes_backend/sticky_notes_backend/settings.py
@@ -153,6 +153,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
 REST_AUTH = {"USE_JWT": True, "JWT_AUTH_HTTPONLY": False}
