e967b09fdd66ff6e3209be190912ca270678e1cf
Haikal Yusuf <itshaikalhere@hotmail.sg>
2024-05-15 22:57:34 +0800

    Add drf-spectacular to Django project

--- sticky-notes-fullstack/sticky_notes_backend/sticky_notes_backend/settings.py
+++ sticky-notes-fullstack/sticky_notes_backend/sticky_notes_backend/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     "allauth.account",
     "allauth.socialaccount",
     "dj_rest_auth.registration",
+    "drf_spectacular",
     # local
     "stickynotes.apps.StickynotesConfig",
 ]
