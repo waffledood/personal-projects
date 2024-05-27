f906cfd3e6038fa44ef5ee514df7dd9fab09027f
Haikal Yusuf <itshaikalhere@hotmail.sg>
2024-05-27 22:30:44 +0800

    Add useDrag to StickyNote

--- sticky-notes-fullstack/sticky-notes-frontend/src/components/StickyNote.js
+++ sticky-notes-fullstack/sticky-notes-frontend/src/components/StickyNote.js
@@ -2,9 +2,13 @@ import { useState } from "react";
 
 import "./StickyNote.css";
 
+import useDrag from "../hooks/useDrag";
+
 function StickyNote({ id, color, xCoord, yCoord, children }) {
-  const [x, setX] = useState(xCoord);
-  const [y, setY] = useState(yCoord);
+  const startingPosition = { startingXCoord: xCoord, startingYCoord: yCoord };
+
+  const { picturePosition, handleMouseDown, handleMouseMove, handleMouseUp } =
+    useDrag(startingPosition);
 
   return (
     <div
@@ -12,9 +16,12 @@ function StickyNote({ id, color, xCoord, yCoord, children }) {
       data-id={`${id}`}
       className={`sticky-note ${color}`}
       style={{
-        top: y,
-        left: x,
+        ...picturePosition,
       }}
+      onMouseDown={handleMouseDown}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseMove}
+      onMouseUp={handleMouseUp}
     >
       <div
         id={`sticky-note-${id}-header`}
