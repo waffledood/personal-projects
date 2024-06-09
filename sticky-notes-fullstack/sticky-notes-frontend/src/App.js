bebba26a0db310e7b40d9d8f95c9ab06b17179d0
Haikal Yusuf <itshaikalhere@hotmail.sg>
2024-06-09 13:55:07 +0800

    Set up react router in App.js

--- sticky-notes-fullstack/sticky-notes-frontend/src/App.js
+++ sticky-notes-fullstack/sticky-notes-frontend/src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import "./App.css";
 
 import StickyNoteBoard from "./components/StickyNoteBoard";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <StickyNoteBoard />,
+    },
+  ]);
+
   return (
     <React.Fragment>
-      <StickyNoteBoard />
+      <RouterProvider router={router} />
     </React.Fragment>
   );
 }
