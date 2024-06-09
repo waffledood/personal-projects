3f754333b592eff6aa1ffb282f287698a0e7f93b
Haikal Yusuf <itshaikalhere@hotmail.sg>
2024-06-09 16:03:31 +0800

    Define Login.js

---
+++ sticky-notes-fullstack/sticky-notes-frontend/src/components/auth/Login.js
@@ -0,0 +1,9 @@
+function Login() {
+  /*
+    TODO - Implement login page
+    Consider copying https://mui.com/material-ui/getting-started/templates/sign-in-side/
+  */
+  return <div>Login page</div>;
+}
+
+export default Login;
