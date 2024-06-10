1e5b6017a99d0bd68837508524c05a5446b6eb5e
Haikal Yusuf <itshaikalhere@hotmail.sg>
2024-06-10 17:56:06 +0800

    Define Login.js

--- sticky-notes-fullstack/sticky-notes-frontend/src/components/auth/Login.js
+++ sticky-notes-fullstack/sticky-notes-frontend/src/components/auth/Login.js
@@ -1,9 +1,88 @@
+import { useEffect, useRef, useState } from "react";
+
+import "./Login.css";
+
 function Login() {
-  /*
-    TODO - Implement login page
-    Consider copying https://mui.com/material-ui/getting-started/templates/sign-in-side/
-  */
-  return <div>Login page</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const usernameRef = useRef();
+  const errRef = useRef();
+
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    usernameRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    setErrMsg("");
+  }, [username, password]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // clear username & password fields
+    setUsername("");
+    setPassword("");
+    setSuccess(true);
+  };
+
+  return (
+    <>
+      {success ? (
+        <section>
+          <h1>You are logged in!</h1>
+          <br />
+          <p>
+            <a href="#">Go to Home</a>
+          </p>
+        </section>
+      ) : (
+        <section>
+          <p
+            ref={errRef}
+            className={errMsg ? "errmsg" : "offscreen"}
+            aria-live="assertive"
+          >
+            {errMsg}
+          </p>
+          <h1>Sign In</h1>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              id="username"
+              ref={usernameRef}
+              autoComplete="off"
+              onChange={(e) => setUsername(e.target.value)}
+              value={username}
+              required
+            />
+
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              required
+            />
+            <button>Sign In</button>
+          </form>
+          <p>
+            Need an Account?
+            <br />
+            <span className="line">
+              {/*put router link here*/}
+              <a href="#">Sign Up</a>
+            </span>
+          </p>
+        </section>
+      )}
+    </>
+  );
 }
 
 export default Login;
