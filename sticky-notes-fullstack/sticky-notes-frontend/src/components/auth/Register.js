67cbf5f23e77cf9e2032141f171868499e0a6a0c
Haikal Yusuf <itshaikalhere@hotmail.sg>
2024-06-14 23:43:46 +0800

    Define Register component

---
+++ sticky-notes-fullstack/sticky-notes-frontend/src/components/auth/Register.js
@@ -0,0 +1,87 @@
+import { useEffect, useRef, useState } from "react";
+
+function Register() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const usernameRef = useRef();
+  const errRef = useRef();
+
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    usernameRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    setErrMsg("");
+  }, [username, password]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <section>
+      <p
+        ref={errRef}
+        className={errMsg ? "errmsg" : "offscreen"}
+        aria-live="assertive"
+      >
+        {errMsg}
+      </p>
+      <h1>Sign Up</h1>
+      <form onSubmit={handleSubmit}>
+        <div class="input-layout">
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            ref={usernameRef}
+            autoComplete="off"
+            onChange={(e) => setUsername(e.target.value)}
+            value={username}
+            required
+          />
+        </div>
+
+        <div class="input-layout">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            autoComplete="off"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            required
+          />
+        </div>
+
+        <div class="input-layout">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            required
+          />
+        </div>
+
+        <button>Sign In</button>
+      </form>
+      <p>
+        Need an Account?
+        <br />
+        <span className="line">
+          {/*put router link here*/}
+          <a href="#">Sign Up</a>
+        </span>
+      </p>
+    </section>
+  );
+}
+
+export default Register;
