f0ae1730d2cf9748841a78ca0b1de9c3513e3a41
Haikal Yusuf <itshaikalhere@hotmail.sg>
2024-06-20 23:18:51 +0800

    Update correct endpoint for token refresh

--- sticky-notes-fullstack/sticky-notes-frontend/src/hooks/useRefreshToken.js
+++ sticky-notes-fullstack/sticky-notes-frontend/src/hooks/useRefreshToken.js
@@ -5,7 +5,7 @@ const useRefreshToken = () => {
   const { setAuth } = useAuth();
 
   const refresh = async () => {
-    const response = await axios.get("/refresh", {
+    const response = await axios.get("/token/refresh/", {
       withCredentials: true,
     });
     setAuth((prev) => {
