7568452163a3df7e42b24cad6fc1c76339e6c695
Haikal Yusuf <itshaikalhere@hotmail.sg>
2024-06-26 08:23:51 +0800

    Update useRefreshToken to include the refreshToken from AuthContext in POST payload

--- sticky-notes-fullstack/sticky-notes-frontend/src/hooks/useRefreshToken.js
+++ sticky-notes-fullstack/sticky-notes-frontend/src/hooks/useRefreshToken.js
@@ -2,12 +2,18 @@ import axios from "../components/api/axios";
 import useAuth from "./useAuth";
 
 const useRefreshToken = () => {
-  const { setAuth } = useAuth();
+  const { auth, setAuth } = useAuth();
 
   const refresh = async () => {
-    const response = await axios.post("/token/refresh/", {
-      withCredentials: true,
-    });
+    const response = await axios.post(
+      "/token/refresh/",
+      {
+        refresh: auth?.refreshToken,
+      },
+      {
+        withCredentials: true,
+      }
+    );
     setAuth((prev) => {
       console.log(JSON.stringify(prev));
       console.log(response.data.access);
