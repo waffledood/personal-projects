14bf203a58733e23f6e81ecde2b623ae4ceabcb5
Haikal Yusuf <itshaikalhere@hotmail.sg>
2024-06-28 17:37:02 +0800

    Use useAxiosPrivate hook to retrieve user's notes

--- sticky-notes-fullstack/sticky-notes-frontend/src/components/StickyNoteBoard.js
+++ sticky-notes-fullstack/sticky-notes-frontend/src/components/StickyNoteBoard.js
@@ -1,18 +1,50 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import useAxiosPrivate from "../hooks/useAxiosPrivate";
 
 import StickyNoteContainer from "./StickyNoteContainer";
 import AddStickyNoteIcon from "./AddStickyNoteIcon";
 
 function StickyNoteBoard() {
-  const [stickyNotes, addStickyNotes] = useState([]);
+  const [stickyNotes, setStickyNotes] = useState([]);
+  const axiosPrivate = useAxiosPrivate();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    let isMounted = true;
+    const controller = new AbortController();
+
+    const getStickyNotes = async () => {
+      try {
+        const response = await axiosPrivate.get("/notes/", {
+          signal: controller.signal,
+        });
+
+        console.log(response.data);
+
+        isMounted && setStickyNotes(response.data);
+      } catch (err) {
+        console.error(err);
+        navigate("/login", { state: { from: location }, replace: true });
+      }
+    };
+
+    getStickyNotes();
+
+    return () => {
+      isMounted = false;
+      controller.abort();
+    };
+  }, []);
 
   return (
     <React.Fragment>
       <StickyNoteContainer stickyNotes={stickyNotes} />
       <AddStickyNoteIcon
         stickyNotes={stickyNotes}
-        addStickyNotes={addStickyNotes}
+        setStickyNotes={setStickyNotes}
       />
     </React.Fragment>
   );
