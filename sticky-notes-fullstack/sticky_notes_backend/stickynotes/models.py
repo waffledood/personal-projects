44c51fa7234e752a3c668e03fc471caefc76a46e
Haikal Yusuf <itshaikalhere@hotmail.sg>
2024-06-30 13:18:36 +0800

    Add color field to Note model

--- sticky-notes-fullstack/sticky_notes_backend/stickynotes/models.py
+++ sticky-notes-fullstack/sticky_notes_backend/stickynotes/models.py
@@ -3,10 +3,24 @@ from django.contrib.auth.models import User
 
 
 class Note(models.Model):
+    YELLOW = "yellow"
+    ORANGE = "orange"
+    PINK = "pink"
+    BLUE = "blue"
+    GREEN = "green"
+    COLOR_CHOICES = {
+        YELLOW: "yellow",
+        ORANGE: "orange",
+        PINK: "pink",
+        BLUE: "blue",
+        GREEN: "green",
+    }
+
     text = models.CharField(max_length=128)
     author = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    color = models.CharField(max_length=15, choices=COLOR_CHOICES, default=YELLOW)
     x_pixels_coord = models.IntegerField()
     y_pixels_coord = models.IntegerField()
 
     def __str__(self):
-        return f"Note ({self.id}) [{self.x_pixels_coord}px, {self.y_pixels_coord}px]: {self.text}"
+        return f"Note ({self.id}) ({self.color}) [{self.x_pixels_coord}px, {self.y_pixels_coord}px]: {self.text}"
