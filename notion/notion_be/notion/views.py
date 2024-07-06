7b4db41dc67416b2d58252436b7a1f1226881820
Haikal Yusuf <itshaikalhere@hotmail.sg>
2024-07-06 12:25:57 +0800

    Define API endpoints for the Page model

--- notion/notion_be/notion/views.py
+++ notion/notion_be/notion/views.py
@@ -1,8 +1,8 @@
 from rest_framework import generics
 
-from .models import Block
+from .models import Block, Page
 from .permissions import IsAuthorOrReadOnly
-from .serializers import BlockSerializer
+from .serializers import BlockSerializer, PageSerializer
 
 
 class ListBlock(generics.ListCreateAPIView):
@@ -20,6 +20,26 @@ class DetailBlock(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthorOrReadOnly,)
     queryset = Block.objects.all()
     serializer_class = BlockSerializer
+
+    def perform_update(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class ListPage(generics.ListCreateAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
+    serializer_class = PageSerializer
+
+    def get_queryset(self):
+        return Page.objects.filter(author=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class DetailPage(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
+    queryset = Page.objects.all()
+    serializer_class = PageSerializer
 
     def perform_update(self, serializer):
         serializer.save(author=self.request.user)
