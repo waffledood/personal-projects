5e9717737ebd2dd66b95570a1894792e969f27d8
Haikal Yusuf <itshaikalhere@hotmail.sg>
2024-07-06 11:10:09 +0800

    Define Block model

--- notion/notion_be/notion/models.py
+++ notion/notion_be/notion/models.py
@@ -7,3 +7,26 @@ class Page(models.Model):
     author = models.ForeignKey(to=User, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now=True)
     saved = models.DateTimeField(auto_now_add=True)
+
+
+class Block(models.Model):
+    BLOCK_TYPE_CHOICES = [
+        ("nn", "Empty"),
+        ("h1", "Heading 1"),
+        ("h2", "Heading 2"),
+        ("h3", "Heading 3"),
+        ("tx", "Text"),
+    ]
+
+    page = models.ForeignKey(to=Page, on_delete=models.CASCADE)
+    page_order = models.PositiveIntegerField()
+    type = models.CharField(max_length=2, choices=BLOCK_TYPE_CHOICES, default="nn")
+
+    content = models.CharField()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["page", "page_order"], name="unique_block_order_per_page"
+            )
+        ]
