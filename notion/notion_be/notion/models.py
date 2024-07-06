9d5104ab178b71604089bd9e3f751a3d44e6ff4d
Haikal Yusuf <itshaikalhere@hotmail.sg>
2024-07-06 11:07:43 +0800

    Define Page model

--- notion/notion_be/notion/models.py
+++ notion/notion_be/notion/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Page(models.Model):
+    title = models.CharField(max_length=128)
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now=True)
+    saved = models.DateTimeField(auto_now_add=True)
