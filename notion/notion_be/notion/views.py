98e72b6c2906d7dc6a93b56486789c40deb90673
Haikal Yusuf <itshaikalhere@hotmail.sg>
2024-07-06 12:21:39 +0800

    Define Views for the Block model

--- notion/notion_be/notion/views.py
+++ notion/notion_be/notion/views.py
@@ -1,6 +1,25 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from rest_framework import generics
+
+from .models import Block
+from .permissions import IsAuthorOrReadOnly
+from .serializers import BlockSerializer
 
 
-def index(request):
-    return HttpResponse("hello world!")
+class ListBlock(generics.ListCreateAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
+    serializer_class = BlockSerializer
+
+    def get_queryset(self):
+        return Block.objects.filter(author=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class DetailBlock(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
+    queryset = Block.objects.all()
+    serializer_class = BlockSerializer
+
+    def perform_update(self, serializer):
+        serializer.save(author=self.request.user)
