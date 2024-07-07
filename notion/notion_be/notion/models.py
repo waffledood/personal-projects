1b5ed8101422382d0f6e1fa35975264ae129a874
Haikal Yusuf <itshaikalhere@hotmail.sg>
2024-07-07 11:25:44 +0800

    Define default value for title field in Page model

--- notion/notion_be/notion/models.py
+++ notion/notion_be/notion/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Page(models.Model):
-    title = models.CharField(max_length=128)
+    title = models.CharField(max_length=128, default="Untitled")
     author = models.ForeignKey(to=User, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now=True)
     saved = models.DateTimeField(auto_now_add=True)
