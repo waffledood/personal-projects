5ab526342b72c08a85d7acae15cbd40dfa9ecd9e
Haikal Yusuf <itshaikalhere@hotmail.sg>
2024-07-08 10:16:24 +0800

    Define styling for App component

--- notion/notion-fe/src/App.js
+++ notion/notion-fe/src/App.js
@@ -1,7 +1,13 @@
-import "./App.css";
+import styles from "./App.module.css";
+
+import Page from "./components/Page";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className={styles.app}>
+      <Page />
+    </div>
+  );
 }
 
 export default App;
