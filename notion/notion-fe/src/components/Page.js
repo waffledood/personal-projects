b2f9430c923ecd7bd362b285da18825a2eac3391
Haikal Yusuf <itshaikalhere@hotmail.sg>
2024-07-08 10:16:51 +0800

    Define styling for Page component

--- notion/notion-fe/src/components/Page.js
+++ notion/notion-fe/src/components/Page.js
@@ -1,18 +1,24 @@
 import React, { useRef, useState } from "react";
 
+import Block from "./Block";
+
+import styles from "./Page.module.css";
+
 function Page() {
   const titleRef = useRef();
   const [title, setTitle] = useState();
 
   return (
-    <div>
+    <div className={styles.pageContainer}>
       <h1
         contentEditable={true}
         ref={titleRef}
         onChange={(e) => setTitle(e.target.value)}
+        className={styles.h1}
       >
         {title}
       </h1>
+      <Block />
     </div>
   );
 }
