43d1f889c1fbe7f817c6a1ab197f936c93fe845c
Haikal Yusuf <itshaikalhere@hotmail.sg>
2024-07-13 19:28:56 +0800

    Change svg from plus to plus-lg

--- notion/notion-fe/src/components/Block.js
+++ notion/notion-fe/src/components/Block.js
@@ -26,10 +26,13 @@ function Block() {
             width="16"
             height="16"
             fill={styles.block_functions_add_color}
-            class="bi bi-plus"
+            class="bi bi-plus-lg"
             viewBox="0 0 16 16"
           >
-            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
+            <path
+              fill-rule="evenodd"
+              d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"
+            />
           </svg>
         </button>
       </div>
