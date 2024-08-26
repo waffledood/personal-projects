e1c5d44d6441a25a073551391436400a7f359ad4
Haikal Yusuf <itshaikalhere@hotmail.sg>
2024-08-26 23:20:24 +0800

    Resize main window width if running in dev mode

--- tabvault/main.js
+++ tabvault/main.js
@@ -1,14 +1,20 @@
 const path = require("path");
 const { app, BrowserWindow } = require("electron");
 
+const isDev = process.env.NODE_ENV !== "production";
 const isMac = process.platform === "darwin";
 
 function createMainWindow() {
   const mainWindow = new BrowserWindow({
     title: "TabVault",
-    width: 500,
+    width: isDev ? 1000 : 500,
     height: 600,
   });
+
+  // Open devtools if in dev
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
 
   mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
 }
