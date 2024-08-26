87f5107ba35dfa29a0104b93157604b247dc758e
Haikal Yusuf <itshaikalhere@hotmail.sg>
2024-08-26 22:50:39 +0800

    Define index.js

---
+++ tabvault/main.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const { app, BrowserWindow } = require("electron");
+
+const isMac = process.platform === "darwin";
+
+function createMainWindow() {
+  const mainWindow = new BrowserWindow({
+    title: "TabVault",
+    width: 500,
+    height: 600,
+  });
+
+  mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
+}
+
+app.whenReady().then(() => {
+  createMainWindow();
+});
+
+app.on("window-all-closed", () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
