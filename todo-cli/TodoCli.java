abb01efa55cc6860a51e43b334e43324bbe6aaf9
Haikal Yusuf <itshaikalhere@hotmail.sg>
2024-09-01 14:10:51 +0800

    Define basic structure for TodoCli

--- todo-cli/TodoCli.java
+++ todo-cli/TodoCli.java
@@ -1,4 +1,34 @@
 public class TodoCli {
+
     public static void main(String[] args) {
+        if (args.length >= 1) {
+            String command = args[0];
+
+            System.out.println("Command: " + command);
+
+            switch (command) {
+                case "add":
+                    break;
+
+                case "update":
+                    break;
+
+                case "delete":
+                    break;
+
+                case "mark-in-progress":
+                    break;
+
+                case "mark-done":
+                    break;
+
+                case "list":
+                    break;
+
+                default:
+                    System.out.println("Please enter one of the following commands: add, update, delete, mark-in-progress, mark-done, list");
+                    break;
+            }
+        }
     }
 }
