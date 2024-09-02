dcf1f0c2dbddfee68cdc616273baa422d9f99fa9
Haikal Yusuf <itshaikalhere@hotmail.sg>
2024-09-02 21:43:22 +0800

    Refactor loadTasksFromJson() method to return a dict

--- todo-cli/TodoCli.py
+++ todo-cli/TodoCli.py
@@ -91,9 +91,9 @@ def loadTasksFromJson():
     with open(TASKS_JSON_FILE_PATH) as f:
         tasks = json.load(f)
 
-    tasks = [Task.from_dict(task) for task in tasks]
+    tasksDict = {task.id: task for task in tasks}
 
-    return tasks
+    return tasksDict
 
 
 def main():
