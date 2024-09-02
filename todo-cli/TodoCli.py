89283d97921acd7f1f996fd2267bd553b89f7153
Haikal Yusuf <itshaikalhere@hotmail.sg>
2024-09-02 22:26:15 +0800

    Add intermediary processing function in loadTasksFromJson()

--- todo-cli/TodoCli.py
+++ todo-cli/TodoCli.py
@@ -87,6 +87,7 @@ def initialization():
 def loadTasksFromJson():
     with open(TASKS_JSON_FILE_PATH) as f:
         tasks = json.load(f)
+    tasks = [Task.from_dict(task) for task in tasks]
     TASKS_DICT = {task["id"]: task for task in tasks}
 
     print("TASKS_DICT:", TASKS_DICT)
