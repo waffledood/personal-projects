7a8b281af7579e45a5be1f7a2caf01533c04e050
Haikal Yusuf <itshaikalhere@hotmail.sg>
2024-09-02 22:18:25 +0800

    Refactor reference to Task id in loadTasksFromJson()

--- todo-cli/TodoCli.py
+++ todo-cli/TodoCli.py
@@ -87,8 +87,7 @@ def initialization():
 def loadTasksFromJson():
     with open(TASKS_JSON_FILE_PATH) as f:
         tasks = json.load(f)
-
-    TASKS_DICT = {task.id: task for task in tasks}
+    TASKS_DICT = {task["id"]: task for task in tasks}
 
     print("TASKS_DICT:", TASKS_DICT)
 
