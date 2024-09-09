d9797af569bfd3550649145e463e5d0d0d77bf56
Haikal Yusuf <itshaikalhere@hotmail.sg>
2024-09-09 22:20:02 +0800

    Load in environment variables from .env file

--- weather-api/weatherapi/settings.py
+++ weather-api/weatherapi/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+from environs import Env
 from pathlib import Path
+
+# Load in environment variables from .env file
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-m!28(hg+gm=6nln4uo+bhj1oihsl4cwhijw8(*y!pmm%f_(i*9"
+SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
