4b8889219467869dd8ab559b97159797a762d4a1
Haikal Yusuf <itshaikalhere@hotmail.sg>
2024-09-09 22:45:24 +0800

    Update weather() view to make request to visualcrossing API

--- weather-api/weather/views.py
+++ weather-api/weather/views.py
@@ -13,5 +13,15 @@ env.read_env()
 
 def weather(request, location):
     apiKey = env.str("VISUAL_CROSSING_API_KEY")
+    response = requests.request(
+        "GET",
+        f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{location}?unitGroup=metric&key={apiKey}&contentType=json",
+    )
 
-    return HttpResponse("Hello world!")
+    if response.status_code != 200:
+        return JsonResponse({"message": "Error retrieving request"}, status=400)
+
+    # Parse the results as JSON
+    jsonData = response.json()
+
+    return JsonResponse(jsonData)
