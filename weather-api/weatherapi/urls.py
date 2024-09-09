266bef2a3d9956552b2a70bcad04c802438fbef4
Haikal Yusuf <itshaikalhere@hotmail.sg>
2024-09-09 16:14:21 +0800

    Change root URL for Django weather app

--- weather-api/weatherapi/urls.py
+++ weather-api/weatherapi/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("weather/", include("weather.urls")),
+    path("weather-api/", include("weather.urls")),
 ]
