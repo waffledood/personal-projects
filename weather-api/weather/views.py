f6732d573d95a07ed4f0cc64b657d69395981781
waffledood <itshaikalhere@hotmail.sg>
2024-09-17 08:30:52 +0800

    Implement caching in weather() view

--- weather-api/weather/views.py
+++ weather-api/weather/views.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 import redis
 import requests
@@ -17,6 +18,17 @@ r = redis.Redis(host="localhost", port=6379, decode_responses=True)
 
 def weather(request, location):
 
+    # Retrieve the date at which this method is invoked
+    today = datetime.datetime.today().strftime("%Y-%m-%d")
+
+    # Retrieve location from cache
+    cachedLocationData = r.get(today + "_" + location.lower())
+
+    cachedLocationData = json.loads(cachedLocationData)
+
+    if cachedLocationData != None:
+        return JsonResponse(cachedLocationData)
+
     apiKey = env.str("VISUAL_CROSSING_API_KEY")
     response = requests.request(
         "GET",
@@ -25,6 +37,9 @@ def weather(request, location):
 
     if response.status_code != 200:
         return JsonResponse({"message": "Error retrieving request"}, status=400)
+
+    # Save response to cache
+    r.set(name=today + "_" + location.lower(), value=response.text, ex=12 * 60 * 60)
 
     # Parse the results as JSON
     jsonData = response.json()
